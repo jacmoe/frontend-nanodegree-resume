c420903efbf391fa0db8ca091b5c43f2365926bb
Jacob Moen <jacmoe.dk@gmail.com>
2017-01-10 07:34:35 +0100

    Turned empty sections black

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,34 @@
+var work = {
+  "jobs" : [
+    {
+      "employer" : "employer",
+      "title" : "title",
+      "location" : "location",
+      "dates" : "dates",
+      "description" : "description"
+    }
+  ]
+};
+
+var projects = {
+  [
+    {
+      "title" : "title",
+      "dates" : "dates",
+      "description" : "description",
+      "images" : [
+        "img/one.jpg", "img/two.jpg"
+      ]
+    },
+  ]
+};
+
 var bio = {
   "name" : "Jacob Moen",
   "role" : "Developer",
-  "contact" : {
+  "welcomeMessage" : "Welcome",
+  "bioPic" : "images/me.jpg",
+  "contacts" : {
     "mobile" : "+4522531780",
     "email" : "jacmoe.dk@gmail.com",
     "twitter" : "@jacmoe",
@@ -9,53 +36,29 @@ var bio = {
     "blog" : "https://jacmoe.dk/",
     "location" : "Denmark"
   },
-  "pic" : "images/me.jpg",
-  "message" : "Welcome",
   "skills" : [
     "first", "second", "third"
   ]
 };
 
-var formattedName = HTMLheaderName.replace('%data%', bio.name);
-var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-var formattedMobile = HTMLmobile.replace('%data%', bio.contact.mobile);
-var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
-var formattedTwitter = HTMLtwitter.replace('%data%', bio.contact.twitter);
-var formattedGithub = HTMLgithub.replace('%data%', bio.contact.github);
-var formattedBlog = HTMLblog.replace('%data%', bio.contact.blog);
-var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
-var formattedBioPic = HTMLbioPic.replace('%data%', bio.pic);
-
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-$("#header").append(formattedMobile);
-$("#header").append(formattedEmail);
-$("#header").append(formattedTwitter);
-$("#header").append(formattedGithub);
-$("#header").append(formattedBlog);
-$("#header").append(formattedLocation);
-$("#header").append(formattedBioPic);
-
-var work = {};
-work.position = "work position";
-work.employer = "employer";
-work.years = "years working";
-work.city = "WorkCity";
-
 var education = {
   "schools" : [
     {
       "name" : "School",
-      "city" : "SchoolCity",
-      "degree" : "degree"
+      "location" : "SchoolCity",
+      "degree dates" : "degree",
+      "url" : "http://school.com/",
+      "majors" : [
+        "one", "two", "three"
+      ]
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "title",
+      "school" : "school",
+      "dates" : "dates",
+      "url" : "http://onlinecourse.com"
     }
   ]
 };
-
-education["schools"]["school"] = "school name";
-education["schools"]["years"] = "years educated";
-education["schools"]["city"] = "SchoolCity";
-
-var formattedEmployer = HTMLworkEmployer.replace('%data', work.employer);
-$("#workExperience").append(formattedEmployer);
-$("#education").append(education["school"]);
